58a48f3bc137081fd98805af354faf5b33f48025
Dan <kl19715@bristol.ac.uk>
2023-04-16 11:05:01 +0100

    adding results for SHVR implementation

--- pairwise_results.py
+++ pairwise_results.py
@@ -1,20 +1,18 @@
-data = [
-    ('AA', 'GVWY', 27, 18973),
-    ('AA', 'SHVR', 18721, 279),
-    ('GDX', 'AA', 17305, 1695),
-    ('GDX', 'GVWY', 395, 18605),
-    ('GDX', 'SHVR', 18960, 40),
-    ('GDX', 'ZIC', 17388, 1612),
-    ('GDX', 'ZIP', 6108, 12892),
-    ('GVWY', 'SHVR', 18960, 40),
-    ('ZIC', 'AA', 4583, 14417),
-    ('ZIC', 'GVWY', 0, 19000),
-    ('ZIC', 'SHVR', 15832, 3168),
-    ('ZIP', 'AA', 16420, 2580),
-    ('ZIP', 'GVWY', 40, 18960),
-    ('ZIP', 'SHVR', 18761, 239),
-    ('ZIP', 'ZIC', 18605, 395),
-]
+data = [('AA', 'GVWY', 27, 18973), 
+ ('AA', 'SHVR', 18721, 279), 
+ ('GDX', 'AA', 17305, 1695), 
+ ('GDX', 'GVWY', 395, 18605), 
+ ('GDX', 'SHVR', 18966, 34), 
+ ('GDX', 'ZIC', 17388, 1612), 
+ ('GDX', 'ZIP', 6108, 12892), 
+ ('GVWY', 'SHVR', 18999, 1), 
+ ('ZIC', 'AA', 4583, 14417), 
+ ('ZIC', 'GVWY', 0, 19000), 
+ ('ZIC', 'SHVR', 16207, 2793), 
+ ('ZIP', 'AA', 16420, 2580), 
+ ('ZIP', 'GVWY', 40, 18960), 
+ ('ZIP', 'SHVR', 18727, 273), 
+ ('ZIP', 'ZIC', 18605, 395),]
 
 # Create a dictionary to store the total number of wins for each trading algorithm
 wins = {'AA': 0, 'GVWY': 0, 'SHVR': 0, 'GDX': 0, 'ZIC': 0, 'ZIP': 0}
