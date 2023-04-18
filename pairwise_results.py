6a4406135235402ef3afb4a72631dfe65a2913ef
Dan <kl19715@bristol.ac.uk>
2023-04-19 00:20:30 +0100

    potentially final results

--- pairwise_results.py
+++ pairwise_results.py
@@ -1,4 +1,5 @@
-data = [('AA', 'SHVR', 7451, 11549), ('GDX', 'AA', 11775, 7225), ('GDX', 'GVWY', 419, 18581), ('GDX', 'SHVR', 6691, 12309), ('GDX', 'ZIC', 17033, 1967), ('GDX', 'ZIP', 2521, 16479), ('GVWY', 'SHVR', 17230, 1770), ('ZIC', 'AA', 4858, 14142), ('ZIC', 'GVWY', 0, 19000), ('ZIC', 'SHVR', 1097, 17903), ('ZIP', 'AA', 17065, 1935), ('ZIP', 'GVWY', 943, 18057), ('ZIP', 'SHVR', 10451, 8549), ('ZIP', 'ZIC', 18738, 262)]
+data = [('AA', 'GVWY', 170, 18830), ('AA', 'SHVR', 7451, 11549), ('GDX', 'AA', 11775, 7225), ('GDX', 'GVWY', 419, 18581), ('GDX', 'SHVR', 6691, 12309), ('GDX', 'ZIC', 17033, 1967), ('GDX', 'ZIP', 2521, 16479), ('GVWY', 'SHVR', 17230, 1770), ('ZIC', 'AA', 4858, 
+14142), ('ZIC', 'GVWY', 0, 19000), ('ZIC', 'SHVR', 1097, 17903), ('ZIP', 'AA', 17065, 1935), ('ZIP', 'GVWY', 943, 18057), ('ZIP', 'SHVR', 10451, 8549), ('ZIP', 'ZIC', 18738, 262)]
 # Create a dictionary to store the total number of wins for each trading algorithm
 wins = {'AA': 0, 'GVWY': 0, 'SHVR': 0, 'GDX': 0, 'ZIC': 0, 'ZIP': 0}
 # Create a dictionary to store the number of algorithms beaten by each trading algorithm
